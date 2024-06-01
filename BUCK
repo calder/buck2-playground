54e6af1de37974c2aac529fcff134a7caca3a989
Calder Coalson <caldercoalson@gmail.com>
2024-06-01 10:40:55 -0700

    WIP: Fixup chrono-tz.

--- BUCK
+++ BUCK
@@ -3,6 +3,7 @@ rust_binary(
     srcs = ["src/main.rs"],
     deps = [
         "//third-party:anyhow",
+        "//third-party:polars",
         "//third-party:reqwest",
         "//third-party:tokio",
     ]
--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,16 @@ async fn main() -> Result<()> {
     let ip = reqwest::get("https://api.ipify.org").await?.text().await?;
     println!("Hello from {}", ip);
 
+    use polars::prelude::*;
+
+    let q = LazyCsvReader::new("docs/data/iris.csv")
+        .with_has_header(true)
+        .finish()?
+        .filter(col("sepal_length").gt(lit(5)))
+        .group_by(vec![col("species")])
+        .agg([col("*").sum()]);
+    let df = q.collect()?;
+    println!("{:?}", df);
+
     Ok(())
 }
